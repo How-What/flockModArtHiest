8c6c3917d17b464094ba69933824669036eb4270
Wallace Wong <wallace.w1997@gmail.com>
2021-11-13 05:38:27 -0500

    renames, cleans up file

--- flockmodarthiest.py
+++ flockmodarthiest.py
@@ -1,10 +1,7 @@
 import requests
 import re
-from datetime import datetime
 
-from requests.api import request
-
-basepath= 'https://flockmod.com/gallery/index.php?q=/post/list/user_id=29797/'
+basepath= 'https://flockmod.com/gallery/index.php?q=/post/list/user_id=/'
 flokmod_img_regex = rb'https://flockmod-[^\']*'
 header_regex = rb'<title>No Images Found</title>'
 
@@ -26,19 +23,9 @@ def get_imageurls(html_page):
             imageurl_clean.append(decoded)
     return imageurl_clean
 
-
-#request the page
-r = requests.get(basepath+'1', allow_redirects=False)
-# #gets the images links from the page
-# imageurl_byte = re.findall(flokmod_img_regex, r.content)
-
-
-#decode the html links
 imageurl = []
-
-#get all from 
+#pull picture from each page
 page_number= 1
-
 while if_image_found(pull_from_site(page_number)):
     imageurl += (get_imageurls(pull_from_site(page_number)))
     print("page: {} processed".format(page_number))
@@ -51,21 +38,14 @@ def get_filename_from_header(cd):
     cd = re.sub('\s+','-',cd)
     cd = re.sub(':','-',cd)
     return cd
-    # if not cd:
-    #     lm =r.headers.get('Last-Modified')
-    #     print(lm)
-    #     return lm
-    # fname = re.findall('filename=(.+)', cd)
-    # if len(fname) == 0:
-    #     return datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-    # return fname[0];
+
 
 print("Retreving images")
 
 for image in imageurl:
     r = requests.get(image, allow_redirects=True)
     filename = get_filename_from_header(r.headers.get('Last-Modified'))
-    save_to = 'D:\\images\\'
+    save_to = 'C:\\images\\'
     if filename == None:
         continue
     savepath = save_to + filename + '.png'
